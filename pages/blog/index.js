d54fda2eca37d80970d0b767b26e955b5ad8ff35
Lenmor Larroza Dimanalata <lenmorld@gmail.com>
2021-10-01 08:17:33 -0400

    eslint fixes

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -15,12 +15,12 @@ export default function Blog() {
                 </p>
                 <ul className={styles.list}>
                     <li>
-                        <Link><a href="/blog/some-really-interesting-title">Some really interesting title</a></Link>
+                        <Link href="/blog/some-really-interesting-title"><a>Some really interesting title</a></Link>
                         <br />
                         <small>September 29, 2021</small>
                     </li>
                     <li>
-                        <Link><a href="/blog/you-want-to-read-this-one">You want to read this one</a></Link>
+                        <Link href="/blog/you-want-to-read-this-one"><a>You want to read this one</a></Link>
                         <br />
                         <small>September 27, 2021</small>
                     </li>
