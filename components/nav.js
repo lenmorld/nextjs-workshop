fc9365e7cccc076f281b8d1b4cfbf74891b1dca2
Lenmor Larroza Dimanalata <lenmorld@gmail.com>
2021-10-01 08:54:03 -0400

    styling

--- components/nav.js
+++ components/nav.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Nav() {
     return <nav>
-        <ul style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center'}}> 
+        <ul style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'stretch'}}> 
             <li><Link href="/"><a>Home</a></Link></li>
             <li><Link href="/blog"><a>Blog</a></Link></li>
             <li><Link href="/projects"><a>Projects</a></Link></li>
