4055243d93866fa69e09f72d4c427ac6206845d4
Lenmor Larroza Dimanalata <lenmorld@gmail.com>
2021-10-05 18:40:36 -0400

    add Nav component; use in pages/index.js

---
+++ components/nav.js
@@ -0,0 +1,11 @@
+export default function Nav() {
+    return (
+        <nav>
+            <a href="/">Home</a>
+            <a href="/blog">Blog</a>
+            <a href="/projects">Projects</a>
+            <a href="/about">About</a>
+            <a href="/contact">Contact</a>
+        </nav>
+    )
+}
