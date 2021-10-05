ac0993a884b594c376a0ce07346dd01e3451bbec
Lenmor Larroza Dimanalata <lenmorld@gmail.com>
2021-10-05 18:56:48 -0400

    make nav items a list

--- components/nav.js
+++ components/nav.js
@@ -1,11 +1,13 @@
 export default function Nav() {
     return (
         <nav>
-            <a href="/">Home</a>
-            <a href="/blog">Blog</a>
-            <a href="/projects">Projects</a>
-            <a href="/about">About</a>
-            <a href="/contact">Contact</a>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/blog">Blog</a></li>
+                <li><a href="/projects">Projects</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/contact">Contact</a></li>
+            </ul>
         </nav>
     )
 }
