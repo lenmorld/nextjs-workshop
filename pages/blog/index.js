3005dbcd27f8f4e55996141b992a8d51f88f0b34
Lenmor Larroza Dimanalata <lenmorld@gmail.com>
2021-10-05 18:35:17 -0400

    add blog/index.js

---
+++ pages/blog/index.js
@@ -0,0 +1,11 @@
+export default function Blog() {
+    return (
+        <div>
+        <h1>My blog posts</h1>
+            <ul>
+                <li>My first post</li>
+                <li>My second post</li>
+            </ul>
+        </div>
+    )
+}
