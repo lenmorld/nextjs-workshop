e09976e44ca2a8b589b6258a7d656da7fa92c312
Lenmor Larroza Dimanalata <lenmorld@gmail.com>
2021-10-06 07:39:38 -0400

    blog index - style and content

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -2,13 +2,23 @@ import Link from 'next/link'
 
 import Layout from '../../components/layout'
 
+import styles from './blog.module.css'
+
 export default function Blog() {
     return (
         <Layout>
             <h1>My blog posts</h1>
-            <ul>
-                <li><Link href="/blog/my-first-post"><a>My first post</a></Link></li>
-                <li><Link href="/blog/my-second-post"><a>My second post</a></Link></li>
+            <ul className={styles.postList}>
+                <li>
+                    <Link href="/blog/my-first-post"><a>My first post</a></Link>
+                    <br />
+                    <small>October 1, 2021</small>
+                </li>
+                <li>
+                    <Link href="/blog/my-second-post"><a>My second post</a></Link>
+                    <br />
+                    <small>October 2, 2021</small>
+                </li>
             </ul>
         </Layout>
     )
