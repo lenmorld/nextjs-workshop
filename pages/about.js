9e25e77985192c836881ce1c32ebdb4e121bae0f
Lenmor Larroza Dimanalata <lenmorld@gmail.com>
2021-10-06 08:25:09 -0400

    About page - content and inline styling

--- pages/about.js
+++ pages/about.js
@@ -10,6 +10,18 @@ export default function About() {
                 borderRadius: '50%'
             }} />
             <h1>About me</h1>
+
+            <p>
+                Hey there. I cannot think of anything right now, 
+                so here are some randomly generated phrases. Enjoy. 
+                Fire up your browser show pony we need to get all stakeholders 
+                up to speed and in the right place and eat our own dog food I have 
+                zero cycles for this market-facing. 
+                Big data we need to aspirationalise our offerings products need 
+                full resourcing and support from a cross-functional team in 
+                order to be built, 
+                maintained, and evolved not enough bandwidth.
+            </p>
         </Layout>
     )
 }
