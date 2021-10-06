1b3669bf2b57efe4974b122325d3b02933ff8123
Lenmor Larroza Dimanalata <lenmorld@gmail.com>
2021-10-06 08:06:31 -0400

    About page - add profile photo; inline CSS-in-JS styles

--- pages/about.js
+++ pages/about.js
@@ -3,6 +3,12 @@ import Layout from '../components/layout'
 export default function About() {
     return (
         <Layout>
+            <img src="profile.jpg" style={{
+                width: '150px',
+                height: '150px',
+                objectFit: 'cover',
+                borderRadius: '50%'
+            }} />
             <h1>About me</h1>
         </Layout>
     )
