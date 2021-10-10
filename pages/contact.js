ca079e53834d24124652b461ed046b696686ab16
Lenmor Larroza Dimanalata <lenmorld@gmail.com>
2021-10-10 07:47:21 -0400

    contact and projects pages

--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,52 @@
+import React from 'react'
+
 import Layout from '../components/layout'
+
+import { FaMapMarkerAlt, FaPhoneAlt, FaEnvelope } from 'react-icons/fa'
+
+import styles from './contact.module.css'
 
 export default function Contact() {
     return (
         <Layout>
-            <h1>Contact me</h1>
+            <h1>Contact</h1>
+
+            <div className={styles.container}>
+                <ul className={styles.list}>
+                    <li>
+                        <FaMapMarkerAlt />
+                        <span>Montreal, CA</span>
+                    </li>
+                    <li>
+                        <FaPhoneAlt />
+                        <span>514 123 1234</span>
+                    </li>
+                    <li>
+                        <FaEnvelope />
+                        <span>yourname@example.com</span>
+                    </li>
+                </ul>
+
+                <form className={styles.form}>
+                    <div>
+                        <input type="text" name="name" placeholder="Name" />
+                    </div>
+                    <div>
+                        <input type="text" name="email" placeholder="Email" />
+                    </div>
+                    <div>
+                        <textarea placeholder="Message" name="message" />
+                    </div>
+                    <div>
+                        <button type="submit">
+                            Send
+                        </button>
+                    </div>
+                </form>
+            </div>
+
+   
+
         </Layout>
     )
 }
--- pages/projects.js
+++ pages/projects.js
@@ -1,9 +1,32 @@
 import Layout from '../components/layout'
 
-export default function Projects() {
+export default function About() {
     return (
         <Layout>
-            <h1>Projects</h1>
+             <div>
+                 <h1>Projects</h1>
+                <p>
+                    Here are some of my latest projects. I like doing stuff.
+                </p>
+                <h3>Project one</h3>
+                <p>
+                    Distinctively re-engineer revolutionary meta-services and premium architectures. 
+                    Intrinsically incubate intuitive opportunities and real-time potentialities. 
+                    Appropriately communicate one-to-one technology after plug-and-play networks.
+                </p>
+                <h3>Project two</h3>
+                <p>
+                    Appropriately empower dynamic leadership skills after business portals. 
+                    Globally myocardinate interactive supply chains with distinctive quality vectors. 
+                    Globally revolutionize global sources through interoperable services.
+                </p>
+                <h3>Project three</h3>
+                <p>
+                    Efficiently enable enabled sources and cost effective products. 
+                    Completely synthesize principle-centered information after ethical communities. 
+                    Efficiently innovate open-source infrastructures via inexpensive materials.
+                </p>
+            </div>
         </Layout>
     )
 }
