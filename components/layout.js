51e6b6a088c0664e4cd747c56e42630377a8118f
Lenmor Larroza Dimanalata <lenmorld@gmail.com>
2021-10-10 08:01:48 -0400

    separate Layout css with Home - copy Home.module.cs as components/layout.module.css - use layout.module.css in Layout component - in Home.module.css, remove everything not used by Home (pages/index.js)

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from './layout.module.css'
 
 import Nav from '../components/nav'
 // error-handling
