5b2d7bb1aaefbe0e2d7918d977e577c47c436e11
Lenmor Larroza Dimanalata <lenmorld@gmail.com>
2021-10-10 08:15:17 -0400

    write blog pages in HTML/JSX

--- pages/blog/my-first-post.js
+++ pages/blog/my-first-post.js
@@ -3,7 +3,36 @@ import Layout from '../../components/layout'
 export default function Post1() {
     return (
         <Layout>
-            <h1>First post</h1>
-        </Layout>
+        <div>
+          <h1>First post</h1>
+          <div>October 1, 2021</div>
+          <br />
+          <p>
+          Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. 
+          Override the digital divide with additional <strong>clickthroughs from DevOps</strong>. Nanotechnology immersion along the information highway 
+          will close the loop on focusing solely on the bottom line.
+          </p>
+          <ul>
+            <li>
+              User generated content in real-time will have multiple <strong>touchpoints</strong> for offshoring. 
+              Keeping your <strong>eye on the ball</strong> while performing a deep dive on the <em>start-up</em> mentality 
+              to derive convergence on cross-platform integration.
+            </li>
+            <li>
+              Dynamically procrastinate <strong>B2C</strong> users after installed base benefits.
+              Collaboratively administrate empowered markets via <strong>plug-and-play</strong> networks.
+            </li>
+          </ul>
+          <p>
+              Oh nice, you&apos;re actually reading this. It&apos;s randomly generated sentences, by the way. That&apos;s around a minute of your life you won&apos;t get back, but at least 
+              you learned some corporate tech buzz words lol.
+          </p>
+          <p>
+          Proactively envisioned multimedia based expertise and <strong>cross-media</strong> growth strategies. 
+          Seamlessly visualize quality &quot;intellectual&quot; capital without superior collaboration and <em>idea-sharing</em>. 
+          Holistically pontificate installed base portals after maintainable products.
+          </p>
+        </div>
+      </Layout>
     )
 }
