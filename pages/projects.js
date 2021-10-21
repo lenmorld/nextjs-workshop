85581796a8663dbeb4b023e1a56416d16a637698
Lenmor Larroza Dimanalata <lenmorld@gmail.com>
2021-10-21 19:57:26 -0400

    add media to Projects page - JSX/HTML

--- pages/projects.js
+++ pages/projects.js
@@ -9,22 +9,66 @@ export default function About() {
                     Here are some of my latest projects. I like doing stuff.
                 </p>
                 <h3>Project one</h3>
+                <iframe
+                    width="560"
+                    height="315"
+                    src="https://www.youtube.com/embed/Tn6-PIqc4UM"
+                    title="YouTube video player"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                ></iframe>
                 <p>
                     Distinctively re-engineer revolutionary meta-services and premium architectures. 
                     Intrinsically incubate intuitive opportunities and real-time potentialities. 
                     Appropriately communicate one-to-one technology after plug-and-play networks.
                 </p>
                 <h3>Project two</h3>
+                <iframe
+                    src="https://giphy.com/embed/Ahb9gSd3WZZw9mHycV"
+                    width="460"
+                    height="480"
+                    frameBorder="0"
+                    className="giphy-embed"
+                    allowFullScreen
+                ></iframe>
+                <p>
+                    <a href="https://giphy.com/gifs/roverdotcom-rover-angry-dog-gifofdogs-Ahb9gSd3WZZw9mHycV">
+                    via GIPHY
+                    </a>
+                </p>
                 <p>
                     Appropriately empower dynamic leadership skills after business portals. 
                     Globally myocardinate interactive supply chains with distinctive quality vectors. 
                     Globally revolutionize global sources through interoperable services.
                 </p>
                 <h3>Project three</h3>
+                <audio
+                    src="http://cdn.mos.musicradar.com/audio/samples/atmosphere-demo-loops/Orgonite-05.mp3"
+                    controls="controls"
+                    preload="none"
+                ></audio>
+                <small><a href="https://www.musicradar.com/news/tech/sampleradar-462-free-atmospheric-samples-224397">musicradar.com sample</a></small>
                 <p>
                     Efficiently enable enabled sources and cost effective products. 
                     Completely synthesize principle-centered information after ethical communities. 
                     Efficiently innovate open-source infrastructures via inexpensive materials.
+                </p>
+                <h4>Project four</h4>
+                <iframe
+                    src="https://open.spotify.com/embed/track/4cOdK2wGLETKBW3PvgPWqT"
+                    width="300"
+                    height="380"
+                    frameBorder="0"
+                    allowtransparency="true"
+                    allow="encrypted-media"
+                ></iframe>
+                <p>
+                    Never gonna give you up. Never gonna let you down.
+                    Never gonna run around and desert you.
+                    Never gonna make you cry. Never gonna say goodbye.
+                    Never gonna tell a lie and hurt you.
+                    Youve been rick-rolled lol.
                 </p>
             </div>
         </Layout>
