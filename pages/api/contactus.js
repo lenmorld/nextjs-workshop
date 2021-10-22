84a221524234b40de9f7cbd4b4afdc2ff4a1e4f1
Lenmor Larroza Dimanalata <lenmorld@gmail.com>
2021-10-22 06:23:48 -0400

    simplify email sending

--- pages/api/contactus.js
+++ pages/api/contactus.js
@@ -14,6 +14,13 @@ response:
 }
 */
 
+const SITE_OWNER_EMAIL = "lenmorld2@gmail.com";
+const SITE_OWNER_PASSWORD = "NEXTjs1234!";
+
+if (!SITE_OWNER_EMAIL || !SITE_OWNER_PASSWORD) {
+    throw new Error("Sender email and password or receiver email missing!")
+}
+
 export default function handler(req, res) {
     console.log("body: ", req.body);
 
